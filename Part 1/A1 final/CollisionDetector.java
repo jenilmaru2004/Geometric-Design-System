d418be9b94f22ff01490edf94fdacade5e6a227e
Jenil Maru <115191900+jenilmaru2004@users.noreply.github.com>
2025-01-19 17:07:56 -0500

    Delete Part 1 directory

--- Part 1/A1 final/CollisionDetector.java
+++
@@ -1,6 +0,0 @@
-public interface CollisionDetector {
-    public boolean intersect(Point point);
-    public boolean intersect(LineSeg line);
-    public boolean intersect(Rectangle rect);
-    public boolean intersect(Circle circle);
-}
